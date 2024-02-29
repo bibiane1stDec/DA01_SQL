23e8470a8bd6c1de5e7fb040eda044dd22c5da95
bibiane1stDec <160806471+bibiane1stDec@users.noreply.github.com>
2024-02-29 16:19:56 +0700

    Update practice 5.sql

--- practice 5.sql
+++ practice 5.sql
@@ -65,13 +65,88 @@ on a.page_id=b.page_id
 where liked_date is null
 ORDER BY a.page_id 
 
+--------------------------------------------------------------------------------------
+mid-course
+--- ex 01
+      select distinct replacement_cost
+from film 
+order by replacement_cost 
 
+---ex. 2
+select 
+SUM(case 
+	when replacement_cost between 9.99 and 19.99 then 1 ELSE 0
+	END) AS LOW, 
+SUM(CASE	
+	WHEN replacement_cost between 20.00 and 24.99 THEN 1 ELSE 0
+	END) AS MEDIUM,
+SUM(CASE	
+	WHEN replacement_cost between 25.00 and 29.99 THEN 1 ELSE 0
+	END) AS HIGH
+from film
 
+--- EX. 3 
+SELECT a.title, a.length, c. name as category_name
+FROM FILM as a 
+JOIN public.film_category as b on a.film_id=b.film_id
+join public.category as c on b.category_id=c.category_id
+where c.name in ('Drama', 'Sports')
+order by a.length desc
 
+-- Ex. 4 
+SELECT c.name as category_name,
+COUNT(a.title)
+FROM FILM as a 
+JOIN public.film_category as b on a.film_id=b.film_id
+join public.category as c on b.category_id=c.category_id
+group by c.name
+order by count(a.title) desc
 
+/* ex. 5: 
+bài này em ko ra đáp án là 54, vì susan davis có hai id khác nhau 
+nghĩa là 2 người khác nhau... 
+*/
+select a.actor_id, a.first_name, a.last_name,
+count(f.film_id)
+from actor as a 
+join film_actor as f
+on a.actor_id=f.actor_id 
+group by a.actor_id 
+order by count(f.film_id) desc
 
+---ex. 6 
+select a.address_id,
+a.address,
+from address as a
+left join customer as c  
+on a.address_id = c.address_id 
+where c.customer_id is null
 
+select count(a.address_id)
+from address as a
+left join customer as c  
+on a.address_id = c.address_id 
+where c.customer_id is null
 
+--- ex. 7 
+Select c.city,
+sum(d.amount)
+from customer as a 
+join address as b on a.address_id=b.address_id 
+join city as c on b.city_id= c.city_id
+join payment as d on a.customer_id= d.customer_id 
+where c.city= 'Tallahassee'
+group by c.city 
+order by sum(d.amount) desc
 
-
+---ex. 8
+Select c.city, e.country,
+sum(d.amount)
+from customer as a 
+join address as b on a.address_id=b.address_id 
+join city as c on b.city_id= c.city_id
+join payment as d on a.customer_id= d.customer_id 
+join country as e on c.country_id= e.country_id
+group by c.city , e.country
+order by sum(d.amount) desc
 
